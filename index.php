50dd8ee86e7680dcf16720bd8c29d0bba61d142e
fidelismutale <mutalefidelis@yahoo.com>
2016-07-15 22:27:41 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="css/stylesheet.css">
+<title>Diners Dive In</title>
+</head>
+<body>
+<div id="header">
+   </div>
 
+</body>
+</html>
