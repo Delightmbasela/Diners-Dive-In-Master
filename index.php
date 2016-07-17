c83ca5a6d878d32418c50a2783a120e53d25280e
viQuato <vikwatokamanga@rocketmail.com>
2016-07-17 06:11:49 +0200

    Getting started with Github

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="stylesheet" href="css/stylesheet.css">
-<title>Diners Dive In</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>Diners Dive In</title>
 </head>
 <body>
-  <div class="container">
+<div class="container">
     <div id="header">
-    <nav>
-    <ul>
-      <a>Home</a>
-      <a>About</a>
-      <a>Contact Us</a>
-      <a>Sign up</a>
-      <a>Sign in</a>
-    </ul>
-    </nav>
-  </div>
-  </div>
-  
+        <h1>Diners' Dive In</h1>
+        <nav>
+            <ul>
+                <a>Home</a>
+                <a>About</a>
+                <a>Contact Us</a>
+                <a>Sign up</a>
+                <a>Sign in</a>
+            </ul>
+        </nav>
+    </div>
+</div>
+
+<div id="mid_section" class="container">
+    <div class="container" id="left_section">
+        <h2>Features</h2>
+        <ul>
+            <li><img alt="Restaurants"></li>
+            <li><img alt="Bars"></li>
+            <li><img alt="Night Clubs"></li>
+        </ul>
+    </div>
+    <div class="container" id="right_section">
+        <img alt="Banner">
+    </div>
+</div>
+
 
 </body>
 </html>
