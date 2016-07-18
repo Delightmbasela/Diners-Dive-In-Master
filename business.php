62418f74b05260e0b06739b9d946bb24949e31d7
viQuato <vikwatokamanga@rocketmail.com>
2016-07-18 10:37:37 +0200

    Create business.php

---
+++ business.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Business Registration</title>
+</head>
+<body>
+  <form>
+    ID:<br>
+    Name: <input name="name">
+  </form>
+</body>
+</html>
