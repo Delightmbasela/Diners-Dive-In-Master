7fb214758b9d5163b6c852f79513595152b2a10f
Mbasela <delightmbasela@gmail.com>
2016-07-22 12:29:02 +0100

    Pushing to see if it will be visible to Fidelis & the rest.

---
+++ user sign-up.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+    <title>User sign-up</title>
+</head>
+<body>
+
+<h1>Welcome to Diners Dive In</h1>
+<h2>user sign-up.</h2>
+<form>
+    First name:<br>
+    <input type="text" name="firstname">
+    <br>
+    Last name:<br>
+    <input type="text" name="lastname">
+    <br>
+
+    Enter email:<br>
+    <input type="text" name="enteremail">
+    <br>
+
+    Enter password:<br>
+    <input type="text" name="enterpassword">
+    <br>
+
+    Re-enter password:<br>
+    <input type="text" name="re-enterpassword">
+<br>
+    <br>
+    primary Dining City:<br>
+    <form action="demo_form.asp">
+        <select name="City">
+            <option value="Kitwe">Kitwe</option>
+            <option value="Lusaka">Lusaka</option>
+            <option value="Kasama">Kasama</option>
+            <option value="Ndola">Ndola</option>
+
+        </select>
+        <br>
+        <br>
+        <input type="Submit" value="Create Account">
+
+    </form>
+</body>
+</html>
